1849d1e6746cd0194002b7040cf97b8583fbe276
Franco Bosetti <francobosetti@live.com.ar>
2024-06-12 01:28:22 -0300

    feat: q3 collator

---
+++ api/src/main/java/ar/edu/itba/pod/query3/QueryCollator.java
@@ -0,0 +1,41 @@
+package ar.edu.itba.pod.query3;
+
+import com.hazelcast.mapreduce.Collator;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+public class QueryCollator implements Collator<Map.Entry<String, Double>, List<Map.Entry<String, Double>>> {
+    private final long N;
+
+    public QueryCollator(long N) {
+        if(N <= 0)
+            throw new IllegalArgumentException("N must be greater than 0");
+
+        this.N = N;
+    }
+
+    @Override
+    public List<Map.Entry<String, Double>> collate(Iterable<Map.Entry<String, Double>> values) {
+        double totalRevenue = getTotalRevenue(values);
+
+        List<Map.Entry<String, Double>> entries = new ArrayList<>();
+
+        for (Map.Entry<String, Double> entry : values) {
+            double percentage = entry.getValue() / totalRevenue * 100;
+            entries.add(Map.entry(entry.getKey(), percentage));
+        }
+
+        return entries.stream().sorted(Map.Entry.comparingByValue()).limit(N).toList();
+    }
+
+    private double getTotalRevenue(Iterable<Map.Entry<String, Double>> values) {
+        double revenue = 0;
+        for (Map.Entry<String, Double> entry : values) {
+            revenue += entry.getValue();
+        }
+        return revenue;
+    }
+}
